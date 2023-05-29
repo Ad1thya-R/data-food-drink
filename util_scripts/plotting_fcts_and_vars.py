e61777e96c7e1eab760a40a954b25b76c45ade52
ludlev2 <ludanyi.levente@gmail.com>
2023-05-29 15:44:30 +0200

    revolutionary insights

--- util_scripts/plotting_fcts_and_vars.py
+++ util_scripts/plotting_fcts_and_vars.py
@@ -54,7 +54,7 @@ def plot_boroughs(data):
     plt.grid()
 
     plt.xlabel(YEAR_XLABEL)
-    plt.xticks(np.arange(0,18),years, rotation=90)
+    plt.xticks(np.arange(0,17),years, rotation=90)
 
     for borough in growth_boroughs:
         borough_by_year = data[data['Area name'] == borough]
@@ -81,7 +81,7 @@ def plot_all_boroughs(data):
     plt.grid()
 
     plt.xlabel(YEAR_XLABEL)
-    plt.xticks(np.arange(0, 18), years, rotation=90)
+    plt.xticks(np.arange(0, 17), years, rotation=90)
 
     for borough in west_boroughs:
         borough_by_year = data[data['Area name'] == borough]
@@ -102,6 +102,92 @@ def plot_all_boroughs(data):
     #plt.show()
 
 
+def plot_all_boroughs_earlier(data):
+    """
+    Plots the all boroughs:
+        growth_boroughs (6 East London) in thick colourful lines
+        west_boroughs (the rest) in thin dotted gray lines
+
+    data: pd.DataFrame with following columns (all names as strings):
+            Area code, Area name, 1998, ... , 2001, 2002, ..., 2016, 2017, ..., 2021
+    """
+    years = [str(y) for y in range(1998, 2022)]
+
+
+    columns = ["Area code", "Area name"] + years
+
+    growth_boroughs = ['Greenwich', 'Hackney', 'Newham', 'Tower Hamlets',
+                    'Barking and Dagenham', 'Waltham Forest']
+
+    boroughs = ['City of London', 'Barking and Dagenham', 'Barnet', 'Bexley',
+                'Brent', 'Bromley', 'Camden', 'Croydon', 'Ealing', 'Enfield',
+                'Greenwich', 'Hackney', 'Hammersmith and Fulham', 'Haringey',
+                'Harrow', 'Havering', 'Hillingdon', 'Hounslow', 'Islington',
+                'Kensington and Chelsea', 'Kingston-upon-Thames', 'Lambeth',
+                'Lewisham', 'Merton', 'Newham', 'Redbridge',
+                'Richmond-upon-Thames', 'Southwark', 'Sutton', 'Tower Hamlets',
+                'Waltham Forest', 'Wandsworth', 'Westminster']
+    plt.figure(figsize=FIGSIZE)
+    plt.grid()
+
+    plt.xlabel(YEAR_XLABEL)
+    plt.xticks(np.arange(0, 24), years, rotation=90)
+
+    for borough in west_boroughs:
+        borough_by_year = data[data['Area name'] == borough]
+        if borough_by_year.empty:
+            continue
+        borough_by_year = borough_by_year.drop(
+            ['Area code', 'Area name'], axis=1).values[0]
+        plt.plot(borough_by_year, color='gray', linewidth=1, linestyle=':')
+
+    for borough in growth_boroughs:
+        borough_by_year = data[data['Area name'] == borough]
+        if borough_by_year.empty:
+            continue
+        borough_by_year = borough_by_year.drop(
+            ['Area code', 'Area name'], axis=1).values[0]
+        plt.plot(borough_by_year, label=borough, linewidth=2)
+
+    plt.legend()
+
+def plot_boroughs_earlier(data):
+    """
+    Plots the growth (6 East London) boroughs
+
+    data: pd.DataFrame with following columns (all names as strings):
+            Area code, Area name, 2001, 2002, ..., 2016, 2017
+    """
+    columns = ["Area code", "Area name"] + years
+
+    growth_boroughs = ['Greenwich', 'Hackney', 'Newham', 'Tower Hamlets',
+                    'Barking and Dagenham', 'Waltham Forest']
+
+    boroughs = ['City of London', 'Barking and Dagenham', 'Barnet', 'Bexley',
+                'Brent', 'Bromley', 'Camden', 'Croydon', 'Ealing', 'Enfield',
+                'Greenwich', 'Hackney', 'Hammersmith and Fulham', 'Haringey',
+                'Harrow', 'Havering', 'Hillingdon', 'Hounslow', 'Islington',
+                'Kensington and Chelsea', 'Kingston-upon-Thames', 'Lambeth',
+                'Lewisham', 'Merton', 'Newham', 'Redbridge',
+                'Richmond-upon-Thames', 'Southwark', 'Sutton', 'Tower Hamlets',
+                'Waltham Forest', 'Wandsworth', 'Westminster']
+    plt.figure(figsize=FIGSIZE)
+    plt.grid()
+
+    plt.xlabel(YEAR_XLABEL)
+    plt.xticks(np.arange(0,24),years, rotation=90)
+
+    for borough in growth_boroughs:
+        borough_by_year = data[data['Area name'] == borough]
+        if borough_by_year.empty:
+            continue
+        borough_by_year = borough_by_year.drop(['Area code', 'Area name'], axis=1).values[0]
+        # plot by year
+        plt.plot(borough_by_year, label=borough)
+
+    plt.legend()
+
+
 def plot_stations(data, col, highlight_stations, see_stations):
     """
     TODO: complete docstring
@@ -110,7 +196,7 @@ def plot_stations(data, col, highlight_stations, see_stations):
     plt.grid()
 
     plt.xlabel(YEAR_XLABEL)
-    plt.xticks(np.arange(0,18), years, rotation=90)
+    plt.xticks(np.arange(0,17), years, rotation=90)
 
 
     for station in see_stations:
@@ -135,7 +221,7 @@ def plot_all_stations(data, col, see_stations):
     plt.grid()
 
     plt.xlabel(YEAR_XLABEL)
-    plt.xticks(np.arange(0,18), years, rotation=90)
+    plt.xticks(np.arange(0,17), years, rotation=90)
 
 
     for station in see_stations:
@@ -159,7 +245,7 @@ def plot_stations_changes(data, col, highlight_stations, see_stations):
     plt.grid()
 
     plt.xlabel(YEAR_XLABEL)
-    plt.xticks(np.arange(0,18),years, rotation=90)
+    plt.xticks(np.arange(0,17),years, rotation=90)
 
     for station in see_stations:
         station_by_year = data[station]
